e615a138981148567caef83d919c15e59f2e6580
dance10 <cuong.nguyen99@gmail.com>
2026-04-08 20:03:42 +0700

    Cleanup temporary node scripts

--- fix_format.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-for (let num of [4, 5, 6]) {
-    let p = 'Exam/ielts-reading-' + num + '.html';
-    let c = fs.readFileSync(p, 'utf-8');
-    c = c.replace(/answers:\s*JSON\.stringify\(answersSubmitted\)/g, 'answers: answersSubmitted');
-    fs.writeFileSync(p, c);
-}
